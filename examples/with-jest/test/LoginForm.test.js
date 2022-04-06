7378d133eb9b29c026dd8a938f57324d5a535a6a
Brian Gantzler <brian.gantzler@cardinalhealth.com>
2022-04-06 09:14:04 -0400

    Example of use order

--- examples/with-jest/test/LoginForm.test.js
+++ examples/with-jest/test/LoginForm.test.js
@@ -2,8 +2,40 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { LoginForm } from '../src/LoginForm'
+import { server } from '../src/mocks/server'
+import { rest } from 'msw'
 
-it('allows user to log in', async () => {
+it('allows user to log in (order segment last)', async () => {
+
+  server.use(
+    rest.post('/login', (req, res, ctx) => {
+      const { username } = req.body
+
+      return res(
+          ctx.json({
+            id: 'f79e82e8-c34a-4dc7-a49e-9fadc0979fda',
+            username,
+            firstName: 'John',
+            lastName: 'Maverick',
+          }),
+      )
+    })
+  );
+  server.use(
+      rest.post('/:id', (req, res, ctx) => {
+        const { username } = req.body
+
+        return res(
+            ctx.json({
+              id: 'f79e82e8-c34a-4dc7-a49e-9fadc0979fda',
+              username,
+              firstName: 'John',
+              lastName: 'Doe',
+            }),
+        )
+      }),
+  );
+
   render(<LoginForm />)
 
   await userEvent.type(screen.getByLabelText(/username/i), 'johnUser')
@@ -11,6 +43,49 @@ it('allows user to log in', async () => {
 
   expect(
     await screen.findByText('f79e82e8-c34a-4dc7-a49e-9fadc0979fda'),
+  ).toBeVisible()
+  expect(await screen.findByText('John')).toBeVisible()
+  expect(await screen.findByText('Maverick')).toBeVisible()
+})
+
+it('allows user to log in (order segment first)', async () => {
+  server.use(
+      rest.post('/:id', (req, res, ctx) => {
+        const { username } = req.body
+
+        return res(
+            ctx.json({
+              id: 'f79e82e8-c34a-4dc7-a49e-9fadc0979fda',
+              username,
+              firstName: 'John',
+              lastName: 'Doe',
+            }),
+        )
+      }),
+  );
+
+  server.use(
+      rest.post('/login', (req, res, ctx) => {
+        const { username } = req.body
+
+        return res(
+            ctx.json({
+              id: 'f79e82e8-c34a-4dc7-a49e-9fadc0979fda',
+              username,
+              firstName: 'John',
+              lastName: 'Maverick',
+            }),
+        )
+      })
+  );
+
+  render(<LoginForm />)
+
+  await userEvent.type(screen.getByLabelText(/username/i), 'johnUser')
+  userEvent.click(screen.getByRole('button', { name: /submit/i }))
+
+  expect(
+      await screen.findByText('f79e82e8-c34a-4dc7-a49e-9fadc0979fda'),
   ).toBeVisible()
   expect(await screen.findByText('John')).toBeVisible()
   expect(await screen.findByText('Maverick')).toBeVisible()
